1fab928f17cdb5916db7255a30f2c343d67c8f52
ДА ДА <test@example.com>
2026-05-24 13:46:40 +0100

    Renamed def method: heron_area to mega_area

--- Python.py
+++ Python.py
@@ -1,6 +1,6 @@
 import math
 
-def heron_area(a: float, b: float, c: float) -> float:
+def mega_area(a: float, b: float, c: float) -> float:
     p = (a + b + c) / 2.0
     area = math.sqrt(p * (p - a) * (p - b) * (p - c))
     return area
@@ -10,7 +10,7 @@ def main():
     a = float(input("Vvedite dlinu storony a: "))
     b = float(input("Vvedite dlinu storony b: "))
     c = float(input("Vvedite dlinu storony c: "))
-    area = heron_area(a, b, c)
+    area = mega_area(a, b, c)
     print(f"Ploshchad treugolnika: {area}")
 
 if __name__ == "__main__":
